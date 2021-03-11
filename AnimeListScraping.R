b083763a906fe6ec893162ba96a78fb2b87d8de9
Webstain <ayman.makhoukhi@gmail.com>
2021-03-12 00:58:18 +0100

    Table Scraped

---
+++ AnimeListScraping.R
@@ -0,0 +1,47 @@
+library(rvest)
+library(dplyr)
+library(xml2)
+
+# Using SelectorGadget
+
+  link = "https://myanimelist.net/"
+  page = read_html(link)
+  
+  animetitle = page %>% html_nodes("h3.latest_updated_h3 a") %>% html_text
+  anime_links = page %>% html_nodes("h3.latest_updated_h3 a") %>% 
+    html_attr("href") 
+  
+  # %>% paste("https://myanimelist.net", ., sep="")
+  
+  get_infos = function(anime_link) {
+      anime_link="https://myanimelist.net/anime/40748/Jujutsu_Kaisen_TV"
+      anime_page = read_html(anime_link)
+      score = anime_page %>% html_nodes(".score-label") %>% html_text
+      ranking = anime_page %>% html_nodes(".numbers.ranked strong") %>% html_text
+      popularity = anime_page %>% html_nodes(".numbers.popularity strong") %>% html_text
+      members = anime_page %>% html_nodes(".numbers.members strong") %>% html_text
+      anime_infos = data.frame(score,ranking,popularity,members)
+      return(anime_infos)
+  }
+  
+  infos = sapply(anime_links, FUN = get_infos)
+  animetitle
+  
+  anime_infos = data.frame(animetitle,infos)
+
+  
+  # Tables
+  
+  col_link = "https://www.numbeo.com/cost-of-living/compare_countries_result.jsp?country1=Morocco&country2=Germany"
+  col_page = read_html(col_link)
+  
+  col_table = col_page %>% html_nodes("table") %>% html_table() %>% .[[1]]
+  
+  
+  wiki_link = "https://en.wikipedia.org/wiki/List_of_apple_cultivars"
+  wiki_page = read_html(wiki_link)
+  
+  apple_table = wiki_page %>% html_nodes("table") %>% .[2] %>% 
+    html_table(fill = TRUE) %>% .[[1]]
+  
+  
